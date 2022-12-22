d0c3ed6375561cbb571c529dfd2cf777f95c0b88
casperliu1118 <80327758+casperliu1118@users.noreply.github.com>
2022-12-22 21:32:17 +0800

    好了，終於可以傳訊息了（但不能傳中文）

--- func.py
+++ func.py
@@ -4,29 +4,20 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import func
-import json
-app = Flask(__name__)
-
 channel_secret = "27b6044fca2f0f59f46001283189e291"
 channel_access_token = "0YptVYTNVQbhpRCezn9hCys1U1nLiQyW6WWAI79h0fUhnUPnFigeXKuSkCDGfwMqYEGkz0X3wz8lqtS3Hbcg2eGpm1GnUyjBHpgcMTCe0tcpDxk34PL9EMk8/5tKi/0QRej7A1Jv5JsjbNEwA1YlgAdB04t89/1O/w1cDnyilFU="
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
+# line_bot_api.reply_message(res['events'][0]['replyToken'], 
+#         TextSendMessage(res['events'][0]['message']['text'])
+#     )
 
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    res = json.loads(body)
-
-    func.choice(res)
-    return 'OK'
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+def choice(res):
+    # message ='請選擇：'+ '\n' 
+    # + '(1)選項一：chatGPT'+'\n'
+    # + '(2)選項二：我'
+    message = 'hello'
+    line_bot_api.reply_message(res['events'][0]['replyToken'], 
+        TextSendMessage(message)
+    )
