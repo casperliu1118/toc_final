797c05cd8ba4456bcbb75ab025a29d55a0affc4c
casperliu1118 <80327758+casperliu1118@users.noreply.github.com>
2022-12-22 20:21:52 +0800

    連網功能

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+LINE_CHANNEL_ACCESS_TOKEN = "0YptVYTNVQbhpRCezn9hCys1U1nLiQyW6WWAI79h0fUhnUPnFigeXKuSkCDGfwMqYEGkz0X3wz8lqtS3Hbcg2eGpm1GnUyjBHpgcMTCe0tcpDxk34PL9EMk8/5tKi/0QRej7A1Jv5JsjbNEwA1YlgAdB04t89/1O/w1cDnyilFU="
 
+LINE_CHANNEL_SECRET = "27b6044fca2f0f59f46001283189e291"
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'django_chatbot.apps.DjangoChatbotConfig',
 ]
 
 MIDDLEWARE = [
